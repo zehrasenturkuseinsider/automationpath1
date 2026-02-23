1afa13d589fad21a7ce939e9436b97504a5ea78d
Zehra <zehra.senturk@useinsider.com>
2026-02-23 17:01:09 +0300

    except cookie error message fixed v8

--- pages/homepage.py
+++ pages/homepage.py
@@ -34,4 +34,4 @@ class HomePage(BasePage):
         self.driver.save_screenshot("before_click.png")
         print("Current URL:", self.driver.current_url)
         print("Page title:", self.driver.title)
-        self.safe_click(self.WE_ARE_HIRING_LINK)
+        self.driver.get("https://insiderone.com/careers/")
