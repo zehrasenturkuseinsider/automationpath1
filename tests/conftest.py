36e069c284a8b0e65e5e52da9a89b0465b5feb47
Zehra <zehra.senturk@useinsider.com>
2026-02-23 16:57:53 +0300

    except cookie error message fixed v7

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,7 @@ def driver(request):
     options.add_argument("--disable-infobars")
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
+    options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
 
     driver = webdriver.Chrome(
